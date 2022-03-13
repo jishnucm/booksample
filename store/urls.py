4a807f9c8a9633720bf1e15da8b07e3b970a89d1
jishnucm <jishnucm55@gmail.com>
2022-03-13 20:28:56 +0530

    template changed

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.books,
-    path('login',views.login,name="login"))
+    path('/', views.books,)
+    path('login',views.login, name="login")
 ]
